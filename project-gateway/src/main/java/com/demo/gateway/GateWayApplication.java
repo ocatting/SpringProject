024d56e8d841d8c99fb74320cbaf4c743eb8bbef
yanxinyu <yanyu429004@qq.com>
2020-07-22 05:58:48 +0800

    测试版本切换

--- project-gateway/src/main/java/com/demo/gateway/GateWayApplication.java
+++ project-gateway/src/main/java/com/demo/gateway/GateWayApplication.java
@@ -12,7 +12,7 @@ public class GateWayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GateWayApplication.class,args);
-        System.out.println("I am dev");
+        System.out.println("I am test");
     }
 
 }
